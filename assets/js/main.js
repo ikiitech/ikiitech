f9550315950e0b7af7b1a65c2f2a7c28c15e07bf
Muhamad Rifki Sumarna <muhammadrifkibelajar05@gmail.com>
2024-02-14 15:15:58 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,23 +137,3 @@
   new PureCounter();
 
 })()
-
-(function() {
-  if (window.addEventListener) {
-    window.addEventListener("load", nascondi_loading_screen, false);
-  } else {
-    window.attachEvent("onload", nascondi_loading_screen);
-  }
-})();
-
-function mostra_loading_screen() {
-  document.getElementById("loading_screen").style.display = 'block';
-}
-
-function nascondi_loading_screen() {
-  setTimeout(function() {
-    document.getElementById("loading_screen").style.display = 'none';
-  }, 3000);
-}
-
-mostra_loading_screen();
