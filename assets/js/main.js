d9e48751d67b02032c6d47876a82cd38297d6544
Muhamad Rifki Sumarna <muhammadrifkibelajar05@gmail.com>
2024-06-05 08:14:46 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,6 +66,22 @@
   }
 
   /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function(e) {
@@ -102,6 +118,16 @@
       }
     }
   });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
 
   /**
    * Hero type effect
